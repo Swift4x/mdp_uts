3ba0bd1493f07b7bc890cbb55a94d7ae99629652
Philip Andrean Henry <colephelps88@gmail.com>
2019-03-24 23:42:26 +0700

    iSTTS MDP 19 216116547 Philip Andrean Henry

---
+++ app/src/main/java/com/example/philipandrean/mdp_uts/row_order.java
@@ -0,0 +1,13 @@
+package com.example.philipandrean.mdp_uts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class row_order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_row_order);
+    }
+}
